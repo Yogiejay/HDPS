39a7ec903644581d5b31f6731d4a1ab02cfa4e6d
Yogiejay <yogi2112001@gmail.com>
2022-04-14 14:19:32 +0530

    done

--- health/views.py
+++ health/views.py
@@ -181,7 +181,7 @@ def preprocess_inputs(df, scaler):
 def prdict_heart_disease(list_data):
     #print(list_data)
     csv_file = Admin_Helath_CSV.objects.get_or_create(id=1)
-    df = pd.read_csv(csv_file.csv_file)
+    df = pd.read_csv(csv_file[0].csv_file)
 
     X = df[['age','sex','cp',  'trestbps',  'chol',  'fbs',  'restecg',  'thalach',  'exang',  'oldpeak',  'slope',  'ca',  'thal']]
     #print(X)
