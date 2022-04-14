073313d9c5ac3435cd98d6b9e97603588b00a096
Yogiejay <yogi2112001@gmail.com>
2022-04-14 16:42:43 +0530

    done

--- health_desease/settings.py
+++ health_desease/settings.py
@@ -77,6 +77,12 @@ WSGI_APPLICATION = 'health_desease.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
         
