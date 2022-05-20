1f21c437a04ea31c80e2a8cba599c251702de4ee
Yogiejay <yogi2112001@gmail.com>
2022-05-20 22:19:23 +0530

    done

--- health_desease/settings.py
+++ health_desease/settings.py
@@ -77,13 +77,13 @@ WSGI_APPLICATION = 'health_desease.wsgi.application'
  #Database
 #https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
        'ENGINE': 'django.db.backends.sqlite3',
        'NAME': BASE_DIR / 'db.sqlite3',
     }
- }
-'''DATABASES = {
+ }'''
+DATABASES = {
     'default': {
         
         'ENGINE': 'django.db.backends.postgresql',
@@ -94,7 +94,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-'''
+
 #
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
